740e3f1dbbefd1727d93618f55451a83e8094e31
Nathan.Gomez <gomezdevlopment@gmail.com>
2022-03-18 12:53:35 -0400

    Create navigation menu and fragments, add theme colors

--- app/src/main/java/com/readandlearn/japanese/MainActivity.java
+++ app/src/main/java/com/readandlearn/japanese/MainActivity.java
@@ -1,8 +1,14 @@
 package com.readandlearn.japanese;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentContainerView;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView nav = findViewById(R.id.bottomNavigationView);
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView);
+        NavController navController = navHostFragment.getNavController();
+        NavigationUI.setupWithNavController(nav, navController);
     }
 }
